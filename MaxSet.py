d1adb7e9e311992389317f86a457c37281aab415
github_username <github_email>
2024-10-28 21:39:00 -0700

    maxset

--- MaxSet.py
+++ MaxSet.py
@@ -1,7 +1,16 @@
 def max_independent_set(nums):
-    '''
-    :param nums:
-    :return:
-    '''
+    """
+    This function will return a list with non-consecutive numbers
+    """
+
+    # Basecase1: If the given list is empty, return an empty list.
+    if nums == 0:
+        return []
+
+    # Basecase2: If all numbers are negative, return an empty list.
+    if all(number < 0 for number in nums):
+        return []
+
+
 
 
